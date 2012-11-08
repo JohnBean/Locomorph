58600eb3fe2fad399c6e40bf3770f2f53b4bde46
Ben <sarukin@gatech.edu>
2012-11-07 22:18:41 -0500

    Shooting refinement, physics-based movement code
    Player can shoot one bullet at a time, pushes cells away and kills
    viruses (two different radiii) upon impact. Player also will no longer
    move through walls, cells, or viruses.

--- DiseaseDestroyer/Assets/Disease Destroyer Assets/Scripts/bulletControl.js
+++ DiseaseDestroyer/Assets/Disease Destroyer Assets/Scripts/bulletControl.js
@@ -1,5 +1,6 @@
-public var radius = 60.0;
-var power = 2.0;
+var pushRadius : float;
+var deathRadius : float;
+var power : float;
 // Use this for initialization
 function Start () {
 }
@@ -18,7 +19,7 @@ function OnCollisionEnter(collision : Collision) {
     		for (var hit : GameObject in colliders) {
             	var diff = (transform.position - hit.rigidbody.position);      	
             	var curDistance = Mathf.Sqrt(( Mathf.Abs(diff.x)*Mathf.Abs(diff.x))+(Mathf.Abs(diff.y)+Mathf.Abs(diff.y))); 
-        		if (curDistance<=radius){
+        		if (curDistance<=deathRadius){
         			hit.GetComponent(virusMovement).kill();
     			}
     		}
@@ -27,20 +28,17 @@ function OnCollisionEnter(collision : Collision) {
     		{
     		
             	diff = (rigidbody.position - hit.rigidbody.position);    	
-            	curDistance = Mathf.Sqrt(( Mathf.Abs(diff.x)*Mathf.Abs(diff.x))+(Mathf.Abs(diff.y)+Mathf.Abs(diff.y))); 
-        		if (curDistance<=radius){
-        			print("cell:"+curDistance + " radius " + radius);
+            	curDistance = Mathf.Sqrt(( Mathf.Abs(diff.x)*Mathf.Abs(diff.x))+(Mathf.Abs(diff.y)*Mathf.Abs(diff.y))); 
+        		if (curDistance<=pushRadius){
+        			print("cell:"+curDistance + " radius " + pushRadius);
            			hit.rigidbody.velocity=hit.rigidbody.velocity+(diff)*-9;//.x=100;//AddExplosionForce(power, explosionPos, radius, 2.0);
     			}
     		}
- 	if(!collision.gameObject.tag.Contains("Player")){
+    //Destroys object in question if not the player or a wall
+ 	if(!collision.gameObject.tag.Contains("Player") && collision.gameObject.tag != "Wall"){
 		Destroy(collision.gameObject);
-    	Destroy (gameObject);
 
-// Removes this script instance from the game object
-		//Destroy (this);
-
-// Removes the rigidbody from the game object
-		//Destroy (rigidbody);
     }
+    //Destroys object
+    Destroy (gameObject);
 }
--- DiseaseDestroyer/Assets/Disease Destroyer Assets/Scripts/playerMovement.js
+++ DiseaseDestroyer/Assets/Disease Destroyer Assets/Scripts/playerMovement.js
@@ -4,55 +4,33 @@ public var maxY = 480;
 public var minY = -480;
 var speed : float;
 var bullet : Rigidbody;
-var bulletSpeed : float = 20;
+var bulletSpeed : float;
 var clone : Transform;
-
-
-
-//temp
-public var radius = 60.0;
-var power = 300.0;
-
 
 // Use this for initialization
 function Start () {
 	
 }
 
-// Update is called once per frame
+//Update is called once per frame
+function Update(){
+	//Finds number of bullets, fires if there are none
+	var numBullets = GameObject.FindGameObjectsWithTag("Bullet").Length;
+	if (Input.GetButtonDown("Fire1") && numBullets == 0) {
+		
+		 // Instantiate the projectile at the position and rotation of this transform
+		 
+		var clone : Rigidbody = Instantiate(bullet, GameObject.Find("BulletSpawn").transform.position, transform.rotation);
+		clone.velocity = Vector3.Normalize(GameObject.Find("BulletSpawn").transform.position - transform.position) * bulletSpeed;
+	}	
+}
+
+// FixedUpdate is called with physics
 function FixedUpdate () {
-	var MouseWorldPosition: Vector3;
 	//if(cPercent>=0 && vPercent>=0){//only move if the game isn't over
 		PlayerFacing();
-		if (Input.GetButtonDown("Fire1")) {
-			
-   			 // Instantiate the projectile at the position and rotation of this transform
-   			 
-   			var clone : Rigidbody = Instantiate(bullet, GameObject.Find("BulletSpawn").transform.position, transform.rotation);
-			clone.velocity = Vector3.Normalize(GameObject.Find("BulletSpawn").transform.position - transform.position) * bulletSpeed;
-    	}
-    	if (Input.GetButtonDown("Fire2")) {
-			var explosionPos : Vector3 = gameObject.transform.position;
-    		var colliders = GameObject.FindGameObjectsWithTag("Finish");
-    		for (var hit : GameObject in colliders) {
-            	var diff = (transform.position - hit.rigidbody.position);      	
-            	var curDistance = Mathf.Sqrt(( Mathf.Abs(diff.x)*Mathf.Abs(diff.x))+(Mathf.Abs(diff.y)+Mathf.Abs(diff.y))); 
-        		if (curDistance<=radius){
-        			hit.GetComponent(virusMovement).kill();
-    			}
-    		}
-    		colliders = GameObject.FindGameObjectsWithTag("Respawn");
-    		for (var hit : GameObject in colliders) {
-            	diff = (transform.position - hit.rigidbody.position);    	
-            	curDistance = Mathf.Sqrt(( Mathf.Abs(diff.x)*Mathf.Abs(diff.x))+(Mathf.Abs(diff.y)+Mathf.Abs(diff.y))); 
-        		if (curDistance<=radius){
-           			hit.rigidbody.AddExplosionForce(power, explosionPos, radius, 2.0);
-    			}
-    		}
-    		print("boom");
-    	}
-    	transform.position.y = transform.position.y + Input.GetAxis("Vertical") * Time.deltaTime * speed;
-    	transform.position.x = transform.position.x + Input.GetAxis("Horizontal") * Time.deltaTime * speed;	
+    	rigidbody.velocity.y =  Input.GetAxis("Vertical") * speed;
+    	rigidbody.velocity.x =  Input.GetAxis("Horizontal") * speed;	
     //}
 
 }
