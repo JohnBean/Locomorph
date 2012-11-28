f3902c94037c71bd4cfde5c35fa2a637baf9e6b8
Ben <sarukin@gatech.edu>
2012-11-27 22:59:52 -0500

    Fleshed out game state machine
    Fleshed out game state machine in HUDhandler - should be easier to
    modify game states and screens now

--- DiseaseDestroyer/Assets/Disease Destroyer Assets/Scripts/HUDHandler.js
+++ DiseaseDestroyer/Assets/Disease Destroyer Assets/Scripts/HUDHandler.js
@@ -56,8 +56,15 @@ function spawnCells(){
 }
 
 function Update () {
-	 //Key inputs for changing game states
-	 if (Input.GetKeyDown("p")){	 	
+	 //Gets stuff for win/lose conditions
+	 var gos : GameObject[];
+	 gos = GameObject.FindGameObjectsWithTag("Respawn"); //How many Cells remain
+     cPercent= (gos.Length-minCells)/(numCellsStart-minCells);
+     gos = GameObject.FindGameObjectsWithTag("Finish"); //finds how many viruses are left
+	 vPercent = gos.Length/numVirusStart;
+	 
+	 //Conditions for changing game states
+	 if (Input.GetKeyDown("p")){	 	//Pausing and unpausing
 	 	print(state);
 	 	if(state == gameState.game){
 	 		state = gameState.pause;
@@ -65,6 +72,13 @@ function Update () {
 	 	else if(state == gameState.pause){
 	 		state = gameState.game;
 	 	}
+	 }
+	 
+	 if(vPercent <= 0){ //Win
+	 	state = gameState.win;
+	 }
+	 if(cPercent <= 0){//Lose
+	 	state = gameState.lose;
 	 }
 	 
 	 //Handling game states
@@ -85,6 +99,8 @@ function Update () {
 	 }
 }
 
+
+
 function OnGUI() {
 	var mapX=590;
 	var mapY=405;
@@ -96,6 +112,41 @@ function OnGUI() {
 	vPercent = gos.Length/numVirusStart;
     
     GUI.skin.font = newFont;
+    
+    //--------------------------WIN AND LOSE SCREENS-----------------------
+    if(state == gameState.win || state == gameState.lose){
+		if(state == gameState.win){
+			GUI.DrawTexture(Rect(Screen.width/2-200,Screen.height/2-200,400,400),winScreen,ScaleMode.ScaleToFit,true,1);
+		}
+		if(state == gameState.lose){
+			GUI.DrawTexture(Rect(Screen.width/2-200,Screen.height/2-200,400,400),loseScreen,ScaleMode.ScaleToFit,true,1);
+		}
+	    if (GUI.Button (Rect(Screen.width/2-50,Screen.height/2+100,100,50), "Play")) {
+	    	gos = GameObject.FindGameObjectsWithTag("Respawn");    
+	    	// Iterate through them and find the closest one
+	   		for (var go : GameObject in gos)  {
+				Destroy(go);
+			}
+			gos = GameObject.FindGameObjectsWithTag("Finish");    
+	    	// Iterate through them and find the closest one
+	   		for (var go : GameObject in gos)  {
+				Destroy(go);
+			}
+			GameObject.FindGameObjectWithTag("Player").GetComponent(playerMovement).restart();
+			state = gameState.game;
+			this.Start();//gameObject.Start();
+		}    
+	}
+    
+    //-------------------PAUSE SCREEN---------------------
+    if(state == gameState.pause){
+    	GUI.DrawTexture(Rect(Screen.width/2-200,Screen.height/2-200,400,400),pauseScreen,ScaleMode.ScaleToFit,true,1);
+    }
+    //-------------------SPLASH SCREEN-------------------------
+    
+    
+    
+    
     
     if(multiplier>1){
     	if(cPercent>=0&&vPercent>=0){
@@ -109,45 +160,19 @@ function OnGUI() {
     		scoreVal= (numVirusStart-gos.Length);
     	}
     }
-    if(cPercent<=0 || vPercent<=0){
-    	if(cPercent>0){
-    		GUI.DrawTexture(Rect(Screen.width/2-200,Screen.height/2-200,400,400),winScreen,ScaleMode.ScaleToFit,true,1);
-    	}
-    	else{
-    		GUI.DrawTexture(Rect(Screen.width/2-200,Screen.height/2-200,400,400),loseScreen,ScaleMode.ScaleToFit,true,1);
-    	}
-    	if (GUI.Button (Rect(Screen.width/2-50,Screen.height/2+100,100,50), "Play")) {
-    		gos = GameObject.FindGameObjectsWithTag("Respawn");    
-    // Iterate through them and find the closest one
-   			for (var go : GameObject in gos)  {
-				Destroy(go);
-			}
-			gos = GameObject.FindGameObjectsWithTag("Finish");    
-    // Iterate through them and find the closest one
-   			for (var go : GameObject in gos)  {
-				Destroy(go);
-			}
-			GameObject.FindGameObjectWithTag("Player").GetComponent(playerMovement).restart();
-			this.Start();//gameObject.Start();
-		}
-    }
-    else{
-    	if(Time.timeScale==0){
-	 		GUI.DrawTexture(Rect(Screen.width/2-200,Screen.height/2-200,400,400),pauseScreen,ScaleMode.ScaleToFit,true,1);
-	 	}
-    	gos = GameObject.FindGameObjectsWithTag("Respawn");   
-    	GUI.Label(Rect(Screen.width/2-300,15,100,100),"SCORE: " + (scoreVal*100)*Mathf.Round(multiplier));//Score
-    	
-    	GUI.backgroundColor = Color(255-(30*cPercent),1-(1*cPercent),1-(1*cPercent));//update color from red to brownred
-   		GUI.HorizontalScrollbar(Rect (Screen.width/2-200,20,200,20), 0, cPercent*100,0, 100);//scale health
-   		GUI.Label(Rect(((Screen.width/4))+50,2,200,200),"CELLS REMAINING: " + gos.length);//cells
-   		GUI.backgroundColor = Color(0, 255-(30*vPercent),2-(2*vPercent),50);//change color based on number of viruses, fade green to teal
-    	GUI.HorizontalScrollbar(Rect (Screen.width/2,20,200,20), 2000, vPercent*100,0, 100);//scale health//change virus bar based on percent remaining
-    	gos = GameObject.FindGameObjectsWithTag("Finish"); //finds how many viruses are left
-    	GUI.Label(Rect(((Screen.width/4)*2)+30,2,200,200),"VIRUSES REMAINING: " + gos.length);//virus
-    	//print(GameObject.FindGameObjectWithTag("Player").GetComponent(playerMovement).slow);
-    	if(GameObject.FindGameObjectWithTag("Player").GetComponent(playerMovement).slow){
-    		GUI.Label(Rect(((Screen.width/2))+0,600,200,200),"SLOWED");//virus
-    	}
-    }
+    
+	gos = GameObject.FindGameObjectsWithTag("Respawn");   
+	GUI.Label(Rect(Screen.width/2-300,15,100,100),"SCORE: " + (scoreVal*100)*Mathf.Round(multiplier));//Score
+	
+	GUI.backgroundColor = Color(255-(30*cPercent),1-(1*cPercent),1-(1*cPercent));//update color from red to brownred
+	GUI.HorizontalScrollbar(Rect (Screen.width/2-200,20,200,20), 0, cPercent*100,0, 100);//scale health
+	GUI.Label(Rect(((Screen.width/4))+50,2,200,200),"CELLS REMAINING: " + gos.length);//cells
+	GUI.backgroundColor = Color(0, 255-(30*vPercent),2-(2*vPercent),50);//change color based on number of viruses, fade green to teal
+	GUI.HorizontalScrollbar(Rect (Screen.width/2,20,200,20), 2000, vPercent*100,0, 100);//scale health//change virus bar based on percent remaining
+	gos = GameObject.FindGameObjectsWithTag("Finish"); //finds how many viruses are left
+	GUI.Label(Rect(((Screen.width/4)*2)+30,2,200,200),"VIRUSES REMAINING: " + gos.length);//virus
+	//print(GameObject.FindGameObjectWithTag("Player").GetComponent(playerMovement).slow);
+	if(GameObject.FindGameObjectWithTag("Player").GetComponent(playerMovement).slow){
+		GUI.Label(Rect(((Screen.width/2))+0,600,200,200),"SLOWED");//virus
+	}
 }
